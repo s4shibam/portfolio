25d9caaa17d179793373b5f24d58122e7f23bf78
s4shibam <shibamsaha2002@gmail.com>
2024-03-23 07:40:22 +0530

    refactor/update: better error handling + add new props

--- src/components/ui/page-heading.tsx
+++ src/components/ui/page-heading.tsx
@@ -7,11 +7,27 @@ import { cn } from '@/utils/functions'
 
 type TPageHeadingProps = {
   className?: string
-  heading: string
+  header?: {
+    heading?: string
+    subHeading?: string
+  }
+  heading?: string
   subHeading?: string
 }
 
-const PageHeading = ({ className, heading, subHeading }: TPageHeadingProps) => {
+const PageHeading = ({
+  className,
+  header,
+  heading,
+  subHeading
+}: TPageHeadingProps) => {
+  heading = heading || header?.heading
+  subHeading = subHeading || header?.subHeading
+
+  if (!heading && !subHeading) {
+    return null
+  }
+
   return (
     <motion.div
       className={cn(
@@ -20,13 +36,17 @@ const PageHeading = ({ className, heading, subHeading }: TPageHeadingProps) => {
       )}
       variants={fadeIn('up', 'tween', 75, 0.1, 0.5)}
     >
-      <h3 className="text-center text-2xl font-bold text-text-black dark:text-text-white sm:text-4xl">
-        {heading}
-        <span className="text-primary dark:text-secondary">.</span>
-      </h3>
-      <h4 className="mx-auto max-w-md text-center text-lg font-semibold text-primary dark:text-secondary sm:text-xl">
-        {subHeading}
-      </h4>
+      {heading && (
+        <h3 className="text-center text-2xl font-bold text-text-black dark:text-text-white sm:text-4xl">
+          {heading}
+          <span className="text-primary dark:text-secondary">.</span>
+        </h3>
+      )}
+      {subHeading && (
+        <h4 className="mx-auto max-w-md text-center text-lg font-semibold text-primary dark:text-secondary sm:text-xl">
+          {subHeading}
+        </h4>
+      )}
     </motion.div>
   )
 }
