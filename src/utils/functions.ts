7837108db2a66ccb16e2c9c7ba468e6aab844cd5
s4shibam <shibamsaha2002@gmail.com>
2024-03-23 10:22:49 +0530

    feat: add function to get project details

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+import { MAJOR_PROJECTS, MINOR_PROJECTS } from '@/constants/projects'
+
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs))
+}
+
+export const getProjectDetails = (projectId?: string) => {
+  if (!projectId) return
+
+  const allProjects = MAJOR_PROJECTS.concat(MINOR_PROJECTS)
+
+  return allProjects.find((project) => project.id === projectId)
 }
