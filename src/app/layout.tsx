0e253de63c2c9c0bb3addcebc4e92a8beda09e3f
s4shibam <shibamsaha2002@gmail.com>
2024-04-10 01:10:49 +0530

    feat: add dynamic metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,71 @@
+import type { Metadata, Viewport } from 'next'
 import { Outfit } from 'next/font/google'
+import { headers } from 'next/headers'
 import { ReactNode } from 'react'
 
-import { METADATA } from '@/constants/meta'
-import LayoutProvider from '@/providers/layout-provider'
-
 import './globals.css'
+import OG_IMAGE from '@/assets/og-image.png'
+import { METADATA } from '@/constants/meta'
+import { APP_URL } from '@/constants/others'
+import { colors } from '@/lib/theme'
+import LayoutProvider from '@/providers/layout-provider'
+import { getProjectDetails } from '@/utils/functions'
 
 const outfit = Outfit({ subsets: ['latin'] })
 
-export const metadata = METADATA
+export async function generateMetadata(): Promise<Metadata> {
+  const metadata: Metadata = {
+    title: METADATA.title,
+    description: METADATA.description,
+    keywords: METADATA.keywords,
+    metadataBase: new URL(APP_URL),
+    openGraph: {
+      images: [
+        {
+          url: OG_IMAGE.src,
+          width: OG_IMAGE.width,
+          height: OG_IMAGE.height,
+          alt: 'Shibam Saha'
+        }
+      ]
+    }
+  }
+  const pathname = new URL(headers().get('x-request-url')!).pathname
+
+  const title =
+    'Shibam Saha' +
+    pathname
+      .split('/')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' > ')
+
+  if (pathname !== '/') {
+    metadata.title = title
+  }
+
+  if (pathname.startsWith('/projects/')) {
+    const projectId = pathname.split('/').pop()
+    const projectDetails = getProjectDetails(projectId)
+    metadata.description = projectDetails?.summary
+
+    if (metadata.openGraph) {
+      metadata.openGraph.images = [
+        {
+          url: projectDetails?.image?.src,
+          width: projectDetails?.image?.width,
+          height: projectDetails?.image?.height,
+          alt: projectDetails?.title
+        }
+      ]
+    }
+  }
+
+  return metadata
+}
+
+export const viewport: Viewport = {
+  themeColor: colors.primary
+}
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
