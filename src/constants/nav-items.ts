1033fa06a555c843834f9f4271a37227cd97fa26
s4shibam <shibamsaha2002@gmail.com>
2024-04-09 19:41:25 +0530

    refactor: comment out snips item

--- src/constants/nav-items.ts
+++ src/constants/nav-items.ts
@@ -1,4 +1,5 @@
 export type TNavItem = {
+  id: string
   title: string
   boxIcon: string
   link: string
@@ -6,26 +7,32 @@ export type TNavItem = {
 
 export const NAV_ITEMS: TNavItem[] = [
   {
+    id: 'projects',
     title: 'Projects',
     boxIcon: 'bx-briefcase',
     link: '/projects'
   },
   {
+    id: 'experiences',
     title: 'Experiences',
     boxIcon: 'bx-trophy',
     link: '/experiences'
   },
   {
+    id: 'stacks',
     title: 'Stacks',
     boxIcon: 'bx-command',
     link: '/stacks'
   },
+  // TODO: Enable when content available
+  // {
+  //   id: 'snips',
+  //   title: 'Snips',
+  //   boxIcon: 'bx-cookie',
+  //   link: '/snips'
+  // },
   {
-    title: 'Snips',
-    boxIcon: 'bx-cookie',
-    link: '/snips'
-  },
-  {
+    id: 'about',
     title: 'About',
     boxIcon: 'bx-user',
     link: '/about'
