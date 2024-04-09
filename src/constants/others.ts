f0e65c2100e5bf1087ad23116e19698e6b4839a3
s4shibam <shibamsaha2002@gmail.com>
2024-04-09 22:51:26 +0530

    fix: change link

--- src/constants/others.ts
+++ src/constants/others.ts
@@ -2,4 +2,4 @@ export const HERO_PAGE_QUOTE = 'Crafting solutions through the art of code'
 
 export const CURRENT_STATUS = 'Presently SDE Intern at Jellybean'
 
-export const RESUME_LINK = 'https://rr.shibamsaha.dev/public'
+export const RESUME_LINK = '/r_public'
