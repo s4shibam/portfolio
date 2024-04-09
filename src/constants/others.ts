37335fb6a9bff47f6c53bc23cd1c71a20ef463b8
s4shibam <shibamsaha2002@gmail.com>
2024-04-10 01:07:56 +0530

    feat: add app url

--- src/constants/others.ts
+++ src/constants/others.ts
@@ -1,3 +1,5 @@
+export const APP_URL = process.env.NEXT_PUBLIC_APP_URL || ''
+
 export const HERO_PAGE_QUOTE = 'Crafting solutions through the art of code'
 
 export const CURRENT_STATUS = 'Presently SDE Intern at Jellybean'
